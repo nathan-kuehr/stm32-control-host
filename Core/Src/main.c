7d6acc282f31679806beda19c4028675cdd306b2
Nathan Kühr <github@kuehrconnect.de>
2026-06-01 16:15:54 +0200

    Adapt project to LwIP

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,7 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
-#include "eth.h"
+#include "lwip.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -93,7 +93,6 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ETH_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
